cb8e110820d649238b8ef9c195376bf5f84fa782
Rafael <rafael.i.porto374@gmail.com>
2020-10-14 21:17:26 -0300

    Implementando QR

--- app/src/main/java/br/com/local/login/MainActivity.java
+++ app/src/main/java/br/com/local/login/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
--- app/src/main/java/br/com/local/login/QrActivity.java
+++ app/src/main/java/br/com/local/login/QrActivity.java
@@ -4,21 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QrActivity extends AppCompatActivity {
     Button btnSair;
+    ImageView ivQR;
+    EditText edtUsu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qr_layout);
+        edtUsu = findViewById(R.id.edtUsu);
+
+        gerarQRcode();
 
         btnSair = findViewById(R.id.btnSair);
 
@@ -28,6 +42,19 @@ public class QrActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    private void gerarQRcode() {
+        String texto = edtUsu.getText().toString();
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+        try {
+            BitMatrix bitMatrix = MultiFormatWriter.encode(texto, BarcodeFormat.QR_CODE,250,250);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            ivQR.setImageBitmap(bitmap);
+        }catch (WriterException e){
+            e.printStackTrace();
+        }
     }
 }
