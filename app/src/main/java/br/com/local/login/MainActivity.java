98f78f2e8d3f7da9cfbe90d7aee704a3bfa1b220
Matheus Santos <matheusantos99@gmail.com>
2021-05-27 18:45:17 -0300

    Conexão com API (Login e Cadastro)

--- app/src/main/java/br/com/local/login/MainActivity.java
+++ app/src/main/java/br/com/local/login/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +14,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.safetynet.SafetyNet;
 import com.google.android.gms.safetynet.SafetyNetApi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks {
     Button btnEntrar, btnPrimeiroA;
     EditText edtUsu, edtSen;
     TextView txtEsqueceu;
     GoogleApiClient googleApiClient;
+    private String URL = "http://192.168.15.28/login_alunos/login.php";
 
     String siteKey = "6LewXq4aAAAAAIa1MvVNm3nLu48omE_RDAPQrAsh";
 
@@ -46,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
         });
 
-        btnEntrar.setOnClickListener(new View.OnClickListener() {
+        /*btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -82,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
             }
 
-        });
+        });*/
 
         btnPrimeiroA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,11 +104,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
         });
 
-        googleApiClient = new GoogleApiClient.Builder(this)
+        /*googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(SafetyNet.API)
                 .addConnectionCallbacks(MainActivity.this)
                 .build();
-        googleApiClient.connect();
+        googleApiClient.connect();*/
 
     }
 
@@ -109,5 +121,46 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionSuspended(int i) {
 
+    }
+
+    public void logar(View view){
+        final String usuario = edtUsu.getText().toString().trim();
+        final String senha = edtSen.getText().toString().trim();
+
+        if (!usuario.equals("") && !senha.equals("")){
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(getApplicationContext(), QrActivity.class);
+                        intent.putExtra("USERLOGIN", usuario);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(MainActivity.this, "RM ou Senha Incorreto", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("rm_aluno", usuario);
+                    data.put("senha_aluno", senha);
+                    return data;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else{
+            Toast.makeText(getApplicationContext(), "Preencha Todos os Campos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/br/com/local/login/PrimeiroAcessoActivity.java
+++ app/src/main/java/br/com/local/login/PrimeiroAcessoActivity.java
@@ -1,21 +1,35 @@
 package br.com.local.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PrimeiroAcessoActivity extends AppCompatActivity {
 
     private Button btnCadastrar, btnVoltarL;
     private EditText edtRmAcesso, edtEmailAcesso, edtConfirmarEmailAcesso, edtSenhaAcesso, edtConfirmarSenhaAcesso;
     private String txtRmAcesso, txtEmailAcesso, txtConfirmarEmailAcesso, txtSenhaAcesso, txtConfiramrSenhaAcesso;
+    private String URL = "http://192.168.15.28/login_alunos/register.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +51,7 @@ public class PrimeiroAcessoActivity extends AppCompatActivity {
             }
         });
 
-        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+        /*btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 txtRmAcesso = edtRmAcesso.getText().toString();
@@ -98,6 +112,56 @@ public class PrimeiroAcessoActivity extends AppCompatActivity {
                 }
             }
         });
+
+         */
+    }
+
+    public void registrar(View view){
+
+        txtRmAcesso = edtRmAcesso.getText().toString();
+        txtEmailAcesso = edtEmailAcesso.getText().toString();
+        txtConfirmarEmailAcesso = edtConfirmarEmailAcesso.getText().toString();
+        txtSenhaAcesso = edtSenhaAcesso.getText().toString();
+        txtConfiramrSenhaAcesso = edtConfirmarSenhaAcesso.getText().toString();
+
+        if(!txtEmailAcesso.equals(txtConfirmarEmailAcesso)){
+            Toast.makeText(getApplicationContext(), "Emails Diferentes", Toast.LENGTH_SHORT).show();
+        }else if (!txtSenhaAcesso.equals(txtConfiramrSenhaAcesso)){
+            Toast.makeText(getApplicationContext(), "Senhas Diferentes", Toast.LENGTH_SHORT).show();
+        }else if (!txtRmAcesso.equals("") && !txtEmailAcesso.equals("") && !txtSenhaAcesso.equals("")){
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Toast.makeText(getApplicationContext(), "Cadastrado com Sucesso", Toast.LENGTH_SHORT).show();
+                        btnCadastrar.setClickable(false);
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(getApplicationContext(), "Algo Está Errado", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email_aluno", txtEmailAcesso);
+                    data.put("senha_aluno", txtSenhaAcesso);
+                    data.put("rm_aluno", txtRmAcesso);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else if(txtRmAcesso.equals("") && txtEmailAcesso.equals("") && txtSenhaAcesso.equals("")){
+            Toast.makeText(getApplicationContext(), "Preencha Todos os Campos", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
